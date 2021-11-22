2cbe3d1a514d1e7552f90c96468eb24d42e83a60
pppratyush.5 <pppratyush.5@gmail.com>
2021-11-22 18:53:08 +0530

    updating rouuter

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import DiscordHome from "./projects/Discord app/DiscordHome";
 import { login, selectUser, logout } from "./features/counter/userSlice";
-import Login from "./common pages/Login";
+import Login from "./commonPages/Login";
 import { auth } from "./firebase";
 import TravelHome from "./projects/travel-advisor/TravelHome";
-
+import AuthProvider from "./contexts/AuthContext";
+import SignUp from "./commonPages/SignUp";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LoginSignupTemplet from "./commonPages/LoginSignupTemplet";
 function App() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
@@ -31,13 +34,16 @@ function App() {
   }, [dispatch]);
   return (
     <div className="app">
-      {user ? (
-        //  <DiscordHome></DiscordHome>
-        <Login></Login>
-      ) : (
-        // <TravelHome></TravelHome>
-        <Login></Login>
-      )}
+      <Router>
+        <AuthProvider>
+          <Switch>
+            <Route path="/discord" component={DiscordHome}></Route>
+            <Route exact path="/login" component={Login} />
+            <Route path="/signUp" component={SignUp}></Route>
+            <Route exact path="/" component={LoginSignupTemplet} />
+          </Switch>
+        </AuthProvider>
+      </Router>
     </div>
   );
 }
