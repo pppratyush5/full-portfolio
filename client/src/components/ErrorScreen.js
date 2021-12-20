52724d016b430f33cfb8a3904eaf5495336f27b6
pppratyush.5 <pppratyush.5@gmail.com>
2021-12-20 22:54:21 +0530

    Error screen changes

---
+++ client/src/components/ErrorScreen.js
@@ -0,0 +1,17 @@
+const ErrorScreen = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+      }}
+    >
+      error...! page not found
+    </div>
+  );
+};
+
+export default ErrorScreen;
